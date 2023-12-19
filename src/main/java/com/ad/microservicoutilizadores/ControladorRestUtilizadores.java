8ed5c897824a2d94ee990f8a28d4ee08828aec44
anacorreia15 <ana.correia2@ipcbcampus.pt>
2023-12-19 19:36:56 +0000

    Adicionamos os ficheiros pom, controladorRestUtilizadores e microservicoUtilizadores

--- src/main/java/com/ad/microservicoutilizadores/ControladorRestUtilizadores.java
+++ src/main/java/com/ad/microservicoutilizadores/ControladorRestUtilizadores.java
@@ -28,6 +28,4 @@ public class ControladorRestUtilizadores {
             return null;
         }
     }
-
-
 }
--- src/main/java/com/ad/microservicoutilizadores/MicroservicoUtilizadores.java
+++ src/main/java/com/ad/microservicoutilizadores/MicroservicoUtilizadores.java
@@ -6,6 +6,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
